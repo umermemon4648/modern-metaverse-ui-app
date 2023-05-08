dcc9efb987536a0ad0e8d39f969f1aefa8ea60cb
umermemon4648 <umermemon4648@gmail.com>
2023-05-09 00:22:00 +0500

    export all sub segments in a Single file

---
+++ src/segments/index.js
@@ -0,0 +1,19 @@
+import About from './About'
+import Explore from './Explore'
+import FeedBack from './FeedBack'
+import GetStarted from './GetStarted'
+import Hero from './Hero'
+import Insights from './Insights'
+import WhatsNew from './WhatsNew'
+import World from './World'
+
+export {
+    About,
+    Explore,
+    FeedBack,
+    GetStarted,
+    Hero,
+    Insights,
+    WhatsNew,
+    World,
+}
