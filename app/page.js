73c5efcf1404db38d253178cb1fb864be0a9679e
umermemon4648 <umermemon4648@gmail.com>
2023-05-09 00:58:53 +0500

    created a separate assests directory

--- app/page.js
+++ app/page.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import Dummy from '../src/assests/index'
 
 const page = () => {
   return (
@@ -11,6 +12,7 @@ const page = () => {
     //   </Head>
     <>
       <p>Hello Dear</p>
+      <Dummy/>
       
       <p className='text-red-700' >Hello! Fellow Developer</p>
     </>
