f9f255a29f2a7de4b725de60a754a865589a9ecd
umermemon4648 <umermemon4648@gmail.com>
2023-05-08 17:09:40 +0500

    added styles and utils folder

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
+// import styles from '@/styles/Home.module.css'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -14,7 +14,9 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={styles.main}>
+
+      <p className='text-red-700' >Hello! Fellow Developer</p>
+      {/* <main className={styles.main}>
         <div className={styles.description}>
           <p>
             Get started by editing&nbsp;
@@ -117,7 +119,11 @@ export default function Home() {
             </p>
           </a>
         </div>
-      </main>
+      </main> */}
+    
+    
+    
+    
     </>
   )
 }
