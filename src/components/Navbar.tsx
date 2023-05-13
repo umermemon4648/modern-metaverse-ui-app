cdf26fe0306c9bf9514e6e376a1870d295224e5d
umermemon4648 <umermemon4648@gmail.com>
2023-05-13 22:49:52 +0500

    feat: done with Navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,10 +1,11 @@
 "use client"
 import React from 'react'
+import Image from 'next/image';
 import {motion} from 'framer-motion'
 import styles from '../styles/index'
 import {navVariants} from '../utils/motion'
-
-
+import searchSVG from '../../public/search.svg'
+import menuSVG from '../../public/menu.svg'
 
 const Navbar = () => {
   return (
@@ -12,10 +13,23 @@ const Navbar = () => {
     variants={navVariants}
     initial = {"hidden"}
     whileInView={"show"}
-    className= {`${styles.xPaddings} py-8 relative`}
+    className= {`${styles.xPaddings} py-8 relative`}>
+      
+      <div className="absolute w-[50%] inset-0 gradient-01"/>
+        <div className={`${styles.innerWidth} mx-auto flex justify-between gap-8`}>
+          <Image src={searchSVG} alt="search" 
+          className='w-[24px] h-[24px] object-contain'
+          />
+          <h2 className='font-extrabold text-[24px] leading-[30px] text-white' >METAVERSUS</h2>
+          <Image src={menuSVG} alt='menuSVG' className='w-[24px] h-[24px] object-contain' />
 
-    >
-      <p className='text-blue-800'>Navbar</p>
+          
+        </div>
+
+
+
+
+
       
     </motion.nav>
   )
--- src/segments/Hero.tsx
+++ src/segments/Hero.tsx
@@ -1,4 +1,9 @@
+"use client"
 import React from 'react'
+import Image from 'next/image'
+import {motion} from 'framer-motion'
+import styles from '../styles/index'
+
 
 const Hero = () => {
   return (
